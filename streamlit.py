d52432a3c86a5974ac21b47e854a71ce0a5ef54b
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-07-03 15:20:41 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,40 @@
 
+import pandas as pd
+import numpy as np
+
+import pytube
+from pydub import AudioSegment
+from io import BytesIO
+import librosa
+import librosa.display
+
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+from glob import glob
+
+st.title('Working with audio file using python')
+st.divider()
+st.subheader('This project aims to search a video on youtube, get the audio and perform some analysis')
+st.divider()
+
+
+# gauth = GoogleAuth()
+# drive = GoogleDrive(gauth)
+
+# # Télécharger un fichier audio
+# audio_file = drive.CreateFile({'title': 'temp.mp4'})
+# audio_file.SetContentFile('temp.mp4')
+# audio_file.Upload()
+# print('Le fichier audio a été téléchargé avec succès.')
+
+# # Lire le fichier audio
+# file_id = audio_file['id']
+# audio_file = drive.CreateFile({'id': file_id})
+# audio_file.GetContentFile('MonFichierAudio.mp3')
+# print('Le fichier audio a été lu avec succès.')
