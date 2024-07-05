24d6f4151c39d9043ba455ff909c54d260fe269c
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-07-05 08:58:25 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,4 @@
+### Libraries
 
 import pandas as pd
 import numpy as np
@@ -23,12 +24,15 @@ from glob import glob
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+### Setting up the page
+
 st.title('Working with audio file using python')
 st.divider()
 st.write('This project aims to search a video on youtube, get the audio and perform some analysis')
 st.divider()
 
-### Function : 
+### Function
+
 def upload_to_drive(file_path):
     gauth = GoogleAuth(settings_file=json.load(StringIO(st.secrets["client_secrets"])))
     drive = GoogleDrive(gauth)
@@ -37,6 +41,7 @@ def upload_to_drive(file_path):
     file_drive.SetContentFile(file_path)
     file_drive.Upload()
 
+### Main app
 
 textinput = st.text_input("Please enter your keywords to find the desired video here", "")
 if len(textinput) > 0 :
@@ -53,9 +58,9 @@ if len(textinput) > 0 :
     buffer=BytesIO()
     audio_stream.stream_to_buffer(buffer)
     buffer.seek(0)
-    yt.streams.filter(only_audio=True).first().download(filename='test.mp4')
-    sound = AudioSegment.from_file("/mount/src/working_with_audio_signal/test.mp4",format="mp4")
-    sound.export("/mount/src/working_with_audio_signal/test.wav", format="wav")
+    upload_to_drive(yt.streams.filter(only_audio=True).first().download(filename='test.mp4'))
+    # sound = AudioSegment.from_file("/mount/src/working_with_audio_signal/test.mp4",format="mp4")
+    # sound.export("/mount/src/working_with_audio_signal/test.wav", format="wav")
     # audio_files = glob('/mount/src/test.wav')
     # y, sr = librosa.load(audio_files[0])
     # st.audio(audio_files)
