b994e740503a7df3e3d1f345121f779eba103a8a
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-08-03 08:31:10 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,10 +26,13 @@ import torch
 from miniaudio import SampleFormat, decode
 ### Setting up the page
 
-st.title('Working with audio file using python')
+st.title('Audio Signal Processing 🎶')
 st.divider()
-st.write('This project aims to search a video on youtube, get the audio and perform some analysis')
+st.write('With this little app, I wanted to dive into signal audio processing.')
+st.write('Depends on your needs, you can either create a sound from scratch using trigonometry or search for a sound on youtube.')
 st.divider()
+
+tab1, tab2 = st.tabs(["Create your own soundwave", "Look for sound on YouTube"])
 
 ### Function
 def convert_mp3_to_wav_ffmpeg_bytes2bytes(input_data: bytes) -> bytes:
@@ -59,23 +62,26 @@ def save_audio(url):
     print(file_name)
     return yt.title, file_name, yt.thumbnail_url
 ### Main app
-
-textinput = st.text_input("Please enter your keywords to find the desired video here", "")
-if len(textinput) > 0 :
-  s = pytube.Search(textinput)
-  url_fixed = "https://www.youtube.com/watch?v="
-  url_moving = str(s.results[0])[-12:]
-  url_total_vid = url_fixed+url_moving
-  
-  st.write("Is that the video you wanted ?")
-  st.video(url_total_vid)
-  if st.button("Yes"):
-      video_title, save_location, video_thumbnail = save_audio(url_total_vid)
-      audio_files = glob(str(save_location))
-      y, sr = librosa.load(audio_files[0])
-      st.write(f'y: {y[:10]}')
-      st.write(f'shape y: {y.shape}')
-      st.write(f'sr: {sr}')
+with tab1 : 
+    st.write("Fun way to understand how signal audio works")
+    st.write("Using trigonometry, you can create audio signal")
+with tab2 :
+    textinput = st.text_input("Please enter your keywords to find the desired video here", "")
+    if len(textinput) > 0 :
+      s = pytube.Search(textinput)
+      url_fixed = "https://www.youtube.com/watch?v="
+      url_moving = str(s.results[0])[-12:]
+      url_total_vid = url_fixed+url_moving
+      
+      st.write("Is that the video you wanted ?")
+      st.video(url_total_vid)
+      if st.button("Yes"):
+          video_title, save_location, video_thumbnail = save_audio(url_total_vid)
+          audio_files = glob(str(save_location))
+          y, sr = librosa.load(audio_files[0])
+          st.write(f'y: {y[:10]}')
+          st.write(f'shape y: {y.shape}')
+          st.write(f'sr: {sr}')
     # yt = pytube.YouTube(url_total_vid)
     # audio_stream = yt.streams.filter(only_audio=True).first()
     # buffer=BytesIO()
